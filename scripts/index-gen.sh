ae7f34005a4a4b6799bf992643e0ccc28865dada
Mick <arceye@mgware.co.uk>
2016-05-18 16:18:19 +0100

    Scripted indexes for man pages
    index-gen.sh added to generate index docs on the fly at each build
    from a Makefile
    
    docs/index.asciidoc amended to promote reading of docs not asking help
    
    _config.yml edit to switch on incremental builds
    
    Entire docs/components dir deleted as these now are generated in docs/man/man9
    
    Signed-off-by: Mick <arceye@mgware.co.uk>

---
+++ scripts/index-gen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# `scripts/index-gen <man-sub-dir> <man-page-type>`
+# eg. 
+# `scripts/index-gen man9 'HAL Component'
+
+DOC="docs/man/$1/index.asciidoc"
+
+touch $DOC
+echo "---" > $DOC
+echo "---" >> $DOC
+echo ":skip front matter:"  >> $DOC
+
+echo "\n= $2 Man Pages\n\n" >> $DOC
+echo "" >> $DOC
+echo "" >> $DOC
+echo "== Available $2 Man Pages are:" >> $DOC
+echo "" >> $DOC
+for i in docs/man/$1/*.asciidoc ; do
+    compname=$(basename -s .asciidoc "$i") ;
+    if [ $compname != "index" ] ; then        
+	echo "- link:$compname[$compname]\n\n" >> $DOC    
+    fi
+done
+echo "" >> $DOC
+echo "Automatically generated index at: $(date)" >> $DOC
