7c92af2ae11bd1d6146f819cd641c20cf0ac980f
Michael Haberler <git@mah.priv.at>
2016-05-25 22:39:42 +0000

    fix "wiki.machinekit.org" links

--- scripts/repl.sh
+++ scripts/repl.sh
@@ -4,3 +4,4 @@
 rpl -R  "image::" "image::" .
 rpl -R  ":imagesdir: /images" ":imagesdir: /images" .
 rpl -R  "link:/docs" "link:/docs" .
+rpl -R  "http://wiki.linuxcnc.org/" "http://wiki.linuxcnc.org/" .
